506f25606c4fd5004f8989551c717b2ac54fdfa3
brisk24 <krivick.aleksej@yandex.ru>
2023-09-15 12:15:34 +0300

    Настройка формы

--- 2048/Game2048.cs
+++ 2048/Game2048.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace _2048
 {
-    public partial class Form1 : Form
+    public partial class Game2048 : Form
     {
-        public Form1()
+        public Game2048()
         {
             InitializeComponent();
         }
