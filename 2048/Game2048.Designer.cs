705b3317020497d79d2f9989f8adc54bc38fe023
brisk24 <krivick.aleksej@yandex.ru>
2023-09-16 23:51:50 +0300

    Позиция формы

--- 2048/Game2048.Designer.cs
+++ 2048/Game2048.Designer.cs
@@ -35,11 +35,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(550, 450);
+            this.ClientSize = new System.Drawing.Size(610, 538);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Game2048";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2048";
             this.ResumeLayout(false);
 
         }
