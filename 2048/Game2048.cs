582178ca363dd1e1c693311e0b9636ce0c0c0311
brisk24 <krivick.aleksej@yandex.ru>
2023-09-17 00:37:56 +0300

    Динамическое создание лейблов

--- 2048/Game2048.cs
+++ 2048/Game2048.cs
@@ -12,9 +12,36 @@ namespace _2048
 {
     public partial class Game2048 : Form
     {
+        static int size = 4;
+        Label[,] box;
         public Game2048()
         {
             InitializeComponent();
+            InitLabels();
         }
+
+
+        void InitLabels()
+        {
+            box = new Label[size, size];
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                {
+                    box[x, y] = CreateLabel();
+                    tableLayoutPanel1.Controls.Add(box[x, y], x, y);
+                }
+        }
+
+        Label CreateLabel()
+        {
+            Label label = new Label();
+            label.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            label.Dock = System.Windows.Forms.DockStyle.Fill;
+            label.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            label.Text = "-";
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;            
+            return label;
+        }     
     }
 }
