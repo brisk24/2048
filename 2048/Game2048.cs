c43fdbb69891ef3d32f6ecedf3b80dc0451b423b
brisk24 <krivick.aleksej@yandex.ru>
2023-09-17 11:31:48 +0300

    Генерация цвета плашек

--- 2048/Game2048.cs
+++ 2048/Game2048.cs
@@ -10,16 +10,49 @@ using System.Windows.Forms;
 
 namespace _2048
 {
+    public delegate void DelegateShow(int x, int y, int number);
     public partial class Game2048 : Form
     {
         static int size = 4;
         Label[,] box;
+        Dictionary<int, Color> backColors;
         public Game2048()
         {
             InitializeComponent();
+            InitBackColors();
             InitLabels();
+            int j = 2;
+
+            for (int y = 0; y < size; y++)
+                for (int x = 0; x < size; x++)
+                {
+                    Show(x, y, j);
+                    j *= 2;
+                }
         }
 
+        void InitBackColors()
+        {
+            backColors = new Dictionary<int, Color>();
+            backColors.Add(0, this.BackColor);
+
+            byte r = 255;
+            byte g = 224;
+            byte b = 192;
+
+            for (int j = 1; j < 65536 * 2048; j *= 2)
+            {
+                backColors.Add(j, Color.FromArgb(r, g, b));
+                r -= 6;
+                g -= 10;
+                b -= 20;
+            }
+
+
+
+
+
+        }
 
         void InitLabels()
         {
@@ -40,8 +73,15 @@ namespace _2048
             label.Dock = System.Windows.Forms.DockStyle.Fill;
             label.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             label.Text = "-";
-            label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;            
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             return label;
-        }     
+        }
+
+        void Show(int x, int y, int number)
+        {
+            box[x, y].Text = number > 0 ? number.ToString() : "";
+            box[x, y].BackColor = backColors[number];
+        }
+
     }
 }
